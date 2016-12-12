c69677e5adf0704001f56b336f83d3eed1cfb2c2
danielarnost <danielarnost@utexaas.edu>
2016-12-11 18:28:53 -0600

    heroku test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # news-scraper
+MONGODB_URI: mongodb://heroku_1387c058:rnsfbt3rvq7pcst27e0tonthg6@ds133158.mlab.com:33158/heroku_1387c058
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost/scrapedNews");
+mongoose.connect("mongodb://heroku_1387c058:rnsfbt3rvq7pcst27e0tonthg6@ds133158.mlab.com:33158/heroku_1387c058");
 var db = mongoose.connection;
 
 
